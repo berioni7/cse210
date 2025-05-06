e7721146db0fb06999d68442f8e3cd656e6593bc
berioni7 <gberioni@byupathway.edu>
2025-05-06 18:49:03 -0300

    updated

--- week01/Exercise2/Program.cs
+++ week01/Exercise2/Program.cs
@@ -4,6 +4,42 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise2 Project.");
+        Console.Write("What is your grade? ");
+        string grade = Console.ReadLine();
+        float conversion = float.Parse(grade);
+        
+        string letter = "";
+        
+        if (conversion >= 90)
+        {
+            letter = "A";
+        }
+        else if (conversion >= 80)
+        {
+            letter = "B";
+        }
+        else if (conversion >= 70)
+        {
+            letter = "C";
+        }
+        else if (conversion >= 60)
+        {
+            letter = "D";
+        }
+        else
+        {
+            letter = "F";
+        }
+
+        Console.WriteLine($"Your grade is: {letter}");
+
+        if (conversion >= 70)
+        {
+            Console.WriteLine("Congratulations, you've passed!");
+        }
+        else 
+        {
+            Console.WriteLine("You didn't pass. Try again!");
+        }    
     }
 }
