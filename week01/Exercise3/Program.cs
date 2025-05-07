4b0cbb7f72b498a445984c944c6bf7850fab0339
berioni7 <gberioni@byupathway.edu>
2025-05-06 22:50:11 -0300

    updated

--- week01/Exercise3/Program.cs
+++ week01/Exercise3/Program.cs
@@ -4,6 +4,31 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise3 Project.");
+        Console.Write("Write a magic number: ");
+        string number = Console.ReadLine();
+        int answer = int.Parse(number);
+
+        int guess;
+
+        do
+        {
+            Console.Write("Write your guess: ");
+            string input = Console.ReadLine();
+            guess = int.Parse(input);
+
+            if (guess < answer)
+            {
+                Console.WriteLine("Higher.");
+            }
+            else if (guess > answer)
+            {
+                Console.WriteLine("Lower.");
+            }
+            else
+            {
+                Console.WriteLine("You guessed it!");
+            }
+
+        } while (guess != answer);
     }
 }
