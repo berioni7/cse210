2fa4e2892207457135d3041d697516bd75602c41
berioni7 <gberioni@byupathway.edu>
2025-05-24 21:57:34 -0300

    scripture added

--- week03/ScriptureMemorizer/Program.cs
+++ week03/ScriptureMemorizer/Program.cs
@@ -4,6 +4,35 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the ScriptureMemorizer Project.");
+        Console.Clear();
+        Console.WriteLine("Welcome to the Scripture Memorizer!");
+        Reference reference = new Reference("Proverbs", 3, 5, 6);
+        string text = "Trust in the Lord with all thine heart and lean not unto thine own understanding In all thy ways acknowledge him and he shall direct thy paths";
+        
+        Scripture scripture = new Scripture(reference, text);
+
+        while (true)
+        {
+            Console.Clear();
+            Console.WriteLine(scripture.GetDisplayText());
+            Console.WriteLine("\nPress Enter to hide more words or type 'quit' to exit.");
+
+            string input = Console.ReadLine();
+
+            if (input.ToLower() == "quit")
+            {
+                break;
+            }
+
+            scripture.HideRandomWords(3);
+
+            if (scripture.IsCompletelyHidden())
+            {
+                Console.Clear();
+                Console.WriteLine(scripture.GetDisplayText());
+                Console.WriteLine("\nAll the words have been hidden. Program ended.");
+                break;
+            }
+        }
     }
 }
